919d3fdf71a41684e328a84c854fdfebb371c97f
afpedreros <felipe.pedreros94@gmail.com>
2024-08-02 12:05:34 -0500

    feat(Restaurants): add section to homepage

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -61,10 +61,14 @@ export default {
 					from: { height: "var(--radix-accordion-content-height)" },
 					to: { height: "0" },
 				},
+				"marquee-left": {
+					to: { transform: "translateX(-50%)" },
+				},
 			},
 			animation: {
 				"accordion-down": "accordion-down 0.2s ease-out",
 				"accordion-up": "accordion-up 0.2s ease-out",
+				"marquee-left": "marquee-left 12s linear infinite forwards",
 			},
 		},
 	},
